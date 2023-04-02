36630f7744f0418cac32837fa850266e52526194
Edgar de Oliveira <edgomendes@gmail.com>
2023-04-02 15:49:45 -0300

    Urls

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+
+    return HttpResponse("Carina é meu amor")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
